9be35b6cd4428ec6813b3a6925011888215037e5
prosystem-pro <prosystem490@gmail.com>
2025-07-20 18:55:32 -0600

    Interceptor corregido

--- src/app/Servicios/Interceptor-Token.ts
+++ src/app/Servicios/Interceptor-Token.ts
@@ -1,6 +1,8 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { LoginServicioPromesaDeDios } from './PromesaDeDios/Login';
+import { LoginServicioCafeJuanAna } from './CafeJuanAna/Login';
+import { LoginServicioDulceTentacion } from './DulceTentacion/Login';
 import { catchError } from 'rxjs';
 import { throwError } from 'rxjs';
 import { Router } from '@angular/router';
@@ -8,22 +10,27 @@ import { Entorno } from '../../app/Entornos/Entorno';
 
 export const AutorizacionInterceptor: HttpInterceptorFn = (Solicitud, Siguiente) => {
   const LoginPromesaDeDios = inject(LoginServicioPromesaDeDios);
-  // const loginMaria = inject(LoginMariaServicio);
+  const LoginCafeJuanAna = inject(LoginServicioCafeJuanAna);
+  const LoginDulceTentacion = inject(LoginServicioDulceTentacion);
+
   const router = inject(Router);
 
   let token: string | null = null;
   const url = Solicitud.url;
 
-    // Verifica hacia qué API va la solicitud
+  // Verifica hacia qué API va la solicitud
   if (url.includes(Entorno.ApiUrlPromesaDeDios)) {
     token = LoginPromesaDeDios.ObtenerToken();
-  } 
-  // else if (url.includes('apiMaria')) {
-  //   token = loginMaria.ObtenerToken();
-  // }
+  }
+  else if (url.includes(Entorno.ApiUrlCafeJuanAna)) {
+    token = LoginCafeJuanAna.ObtenerToken();
+  }
+  else if (url.includes(Entorno.ApiUrlDulceTentacion)) {
+    token = LoginDulceTentacion.ObtenerToken();
+  }
 
 
-    // Si hay token, adjúntalo
+  // Si hay token, adjúntalo
   if (token) {
     Solicitud = Solicitud.clone({
       setHeaders: {
@@ -36,16 +43,17 @@ export const AutorizacionInterceptor: HttpInterceptorFn = (Solicitud, Siguiente)
     catchError(error => {
       if (error.status === 401) {
         console.warn('Token expirado o inválido');
-        if (url.includes('/PromesaDeDios')) {
+        if (url.includes(Entorno.ApiUrlPromesaDeDios)) {
           LoginPromesaDeDios.EliminarToken();
           router.navigate(['/menu']);
-        } 
-        // else if (url.includes('apiMaria')) {
-        //   loginMaria.EliminarToken();
-        //   router.navigate(['/login-maria']);
-        // }
+        } else if (url.includes(Entorno.ApiUrlCafeJuanAna)) {
+          LoginCafeJuanAna.EliminarToken();
+          router.navigate(['/menu']);
+        }else if (url.includes(Entorno.ApiUrlDulceTentacion)) {
+          LoginDulceTentacion.EliminarToken();
+          router.navigate(['/menu']);
+        }
       }
-
       return throwError(() => error);
     })
   );
